45316ade6a4a58b1e3397419d06499997f0f2a6f
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-08-12 15:30:37 +0200

    Add app constants and error model

---
+++ BackEnd/Helpers/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace BackEnd.Helpers
+{
+
+    /// <summary>
+    /// This class is responsible only for providing constants to all classes/methods etc. accross the application.
+    /// It can be a partial class if necessary; and if so, then put the module in the root folder and additional 
+    /// partials in other project folders.
+    public class Constants
+    {
+
+        public static class Errors
+        { 
+            
+
+
+        
+        }
+
+    }
+
+}
---
+++ BackEnd/Models/Json/ErrorHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BackEnd.Models.Json
+{
+
+    public class ErrorHandler
+    {
+
+        [JsonPropertyName("ErrorDesc")]
+        public string ErrorDesc { get; set; }
+
+        [JsonPropertyName("ErrorCode")]
+        public string ErrorCode { get; set; }
+
+        public ErrorHandler()
+        {
+            ErrorDesc = "n/a";
+            ErrorCode = "no_errors_found";
+        }
+
+    }
+
+}

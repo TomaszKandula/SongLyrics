fe79f46d9b6fa3d7f93bd6093c90c411ceafe98e
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-08-12 22:54:52 +0200

    Add properties to LogicContext/ILogicContext

--- BackEnd/Logic/ILogicContext.cs
+++ BackEnd/Logic/ILogicContext.cs
@@ -1,11 +1,13 @@
-﻿namespace BackEnd.Logic
+﻿using BackEnd.Logic.Music;
+using BackEnd.Logic.Artists;
+
+namespace BackEnd.Logic
 {
 
     public interface ILogicContext
     {
-
-        
-
+        IArtists Artists { get; }
+        IMusic Music { get; }
     }
 
 }
--- BackEnd/Logic/LogicContext.cs
+++ BackEnd/Logic/LogicContext.cs
@@ -1,10 +1,56 @@
-﻿namespace BackEnd.Logic
+﻿using BackEnd.Logic.Music;
+using BackEnd.Logic.Artists;
+using BackEnd.Models.Database;
+
+namespace BackEnd.Logic
 {
 
     public class LogicContext : ILogicContext
     {
 
+        private readonly MainDbContext FMainDbContext;
+
+        private IArtists FArtists;
+        private IMusic FMusic;
+
+        public LogicContext(MainDbContext AMainDbContext) 
+        {
+            FMainDbContext = AMainDbContext;
+        }
+
+        public IArtists Artists 
+        {
+
+            get 
+            {
+
+                if (FArtists == null) 
+                {
+                    FArtists = new Artists.Artists(FMainDbContext);
+                }
+
+                return FArtists;
+
+            }
+
+        }
+
+        public IMusic Music 
+        {
+
+            get 
+            {
+
+                if (FMusic == null) 
+                {
+                    FMusic = new Music.Music(FMainDbContext);
+                }
+
+                return FMusic;            
+            
+            }
         
+        }
 
     }
 

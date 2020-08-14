6a0ad1e28af47880c0aaa01758acaf3c41ae8d38
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-08-14 12:30:47 +0200

    test content when app logger invoked

--- BackEnd.IntegrationTests/IntegrationTests.cs
+++ BackEnd.IntegrationTests/IntegrationTests.cs
@@ -66,6 +66,11 @@ namespace SongLyricsBackEnd.IntegrationTests
             var LogsPath = AppDomain.CurrentDomain.BaseDirectory + "\\test-logs";
             var FileName = $"log-{string.Format("{0:yyyyMMdd}", DateTime.Now)}.txt";
 
+            var TestTextInfo  = "Integration Tests for SeriLog sitting behind AppLogger (INFO).";
+            var TestTextWarn  = "Integration Tests for SeriLog sitting behind AppLogger (WARN).";
+            var TestTextError = "Integration Tests for SeriLog sitting behind AppLogger (ERROR).";
+            var TestTextFatal = "Integration Tests for SeriLog sitting behind AppLogger (FATAL).";
+
             if (!Directory.Exists(LogsPath))
             {
                 Directory.CreateDirectory(LogsPath);
@@ -87,10 +92,20 @@ namespace SongLyricsBackEnd.IntegrationTests
                 .CreateLogger();
 
             // Act
-            FAppLogger.LogInfo("Integration Tests for SeriLog sitting behind AppLogger.");
+            FAppLogger.LogInfo(TestTextInfo);
+            FAppLogger.LogWarn(TestTextWarn);
+            FAppLogger.LogError(TestTextError);
+            FAppLogger.LogFatality(TestTextFatal);
+
+            // Release file lock and get the content
+            Log.CloseAndFlush();
+            var TestLogFile = File.ReadAllText(LogsPath + "\\" + FileName);
 
             // Assert
-            File.Exists(LogsPath + "\\" + FileName).Should().BeTrue();
+            TestLogFile.Should().Contain(TestTextInfo);
+            TestLogFile.Should().Contain(TestTextWarn);
+            TestLogFile.Should().Contain(TestTextError);
+            TestLogFile.Should().Contain(TestTextFatal);
 
         }
 

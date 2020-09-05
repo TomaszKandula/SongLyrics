c163099ed0e0a8aef536506fc3e541e28d0e11f4
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-09-05 16:28:39 +0200

    refactor, add custom css, add materialize-icons

--- FrontEnd/ClientApp/src/components/views/bandsView.js
+++ FrontEnd/ClientApp/src/components/views/bandsView.js
@@ -7,17 +7,70 @@ export class BandsView extends Component
     {
 
         return (
+
             <div>
 
-                <div>
-                    Search
+                <div class="row">
+
+                    <div class="col s2">
+                        &nbsp;
+                    </div>
+
+                    <div class="col s9">
+
+                        <div class="row">
+
+                            <div class="col s8">
+
+                                <div class="row" id="topbarsearch">
+                                    <div class="input-field col s6 s12 black-text">
+                                        <i class="grey-text material-icons prefix">search</i>
+                                        <input type="text" placeholder="Search for band" id="autocomplete-input" class="autocomplete black-text" />
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+
+                            <table class="highlight">
+                                <thead>
+                                    <tr>
+                                        <th>Lp</th>
+                                        <th>Band Name</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Queen</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Led Zeppelin</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Metallica</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col s2">
+                        &nbsp;
+                    </div>
+
                 </div>
 
-                <div>
-                    BandsList
-                </div>
+             </div>
 
-            </div>
         );
 
     }

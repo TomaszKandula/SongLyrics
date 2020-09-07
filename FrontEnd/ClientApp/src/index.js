26f00d44da3209487ebca87059c31bd3c1be4448
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-09-07 15:29:50 +0200

    adding react-redux

--- FrontEnd/ClientApp/src/index.js
+++ FrontEnd/ClientApp/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./app";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <BrowserRouter basename={baseUrl}>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter basename={baseUrl}>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     rootElement
 );
-

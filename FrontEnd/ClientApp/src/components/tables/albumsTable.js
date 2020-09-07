63b58cd3072b3e58262a2b6c7346ec3267750e5c
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-09-07 12:43:21 +0200

    update albums table

--- FrontEnd/ClientApp/src/components/tables/albumsTable.js
+++ FrontEnd/ClientApp/src/components/tables/albumsTable.js
@@ -1,37 +1,76 @@
 ﻿import React, { Component } from "react";
+import * as mockPayLoads from "../../tempMocks/mockPayLoads"; 
 
 export class AlbumsTable extends Component
 {
 
-    render()
+    constructor(props)
+    {
+        super(props);
+        this.state =
+        {
+            albums: [],
+            loading: true
+        };
+    }
+
+    componentDidMount()
+    {
+        //this.getBands();
+        this.mockData();
+    }
+
+    mockData()
+    {
+
+        let jsonResponse = mockPayLoads.albums;
+        let parsedJson = JSON.parse(jsonResponse);
+        let objAblums = parsedJson.Albums;
+
+        this.setState(
+            {
+                albums: objAblums,
+                loading: false
+            });
+
+    }
+
+    renderTable(data)
     {
 
         return (
+
+            <table className="albumsTable">
+                <thead>
+                    <tr>
+                        <th className="albumsTableCol1">Lp</th>
+                        <th className="albumsTableCol2">Album Name</th>
+                        <th className="albumsTableCol3">Issued</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(albums =>
+                        <tr key={albums.Id}>
+                            <td className="albumsTableCol1">{albums.Id}</td>
+                            <td className="albumsTableCol2">{albums.AlbumName}</td>
+                            <td className="albumsTableCol3">{albums.Issued}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            
+        );
+
+    }
+
+    render()
+    {
+
+        let populatedTable = this.state.loading ? <p><em>Loading..., please wait.</em></p> : this.renderTable(this.state.albums);
+
+        return (
             <div>
-
-                <table className="albumsTable">
-                    <thead>
-                        <tr>
-                            <th className="albumsTableCol1">Lp</th>
-                            <th className="albumsTableCol2">Album Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="albumsTableCol1">1</td>
-                            <td className="albumsTableCol2">Queen</td>
-                        </tr>
-                        <tr>
-                            <td className="albumsTableCol1">2</td>
-                            <td className="albumsTableCol2">Queen II</td>
-                        </tr>
-                        <tr>
-                            <td className="albumsTableCol1">3</td>
-                            <td className="albumsTableCol2">Sheer Heart Attack</td>
-                        </tr>
-                    </tbody>
-                </table>
-
+                {populatedTable}
             </div>
         );
 

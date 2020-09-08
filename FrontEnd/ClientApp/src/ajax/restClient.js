25e26f44536d055557b647d4caa5efdb5c6453cf
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-09-08 10:55:55 +0200

    add simple restClient

---
+++ FrontEnd/ClientApp/src/ajax/restClient.js
@@ -0,0 +1,61 @@
+﻿export default class RestClient
+{
+
+    constructor(ACustomToken, AContentType)
+    {
+        this.LCustomToken = ACustomToken;
+        this.LContentType = AContentType;
+    }
+
+    Execute(AMethod, AUrl, APayLoad, ACallback)
+    {
+
+        let LRequest = new XMLHttpRequest();
+        try
+        {
+
+            LRequest.open(AMethod, AUrl, true);
+            LRequest.setRequestHeader("Content-Type", this.LContentType);
+            LRequest.setRequestHeader("X-CSRF-TOKEN", this.LCustomToken);
+
+            LRequest.onload = function ()
+            {
+
+                if (this.status >= 200 && this.status < 400)
+                {
+                    ACallback(this.response, this.status);
+                }
+                else
+                {
+                    ACallback(null, this.status);
+                }
+
+            };
+
+            LRequest.onerror = function ()
+            {
+                ACallback(null, this.status);
+            };
+
+            let LMethod = AMethod.toUpperCase();
+
+            if (LMethod === "GET" || LMethod === "DELETE")
+            {
+                LRequest.send();
+            }
+
+            if (LMethod === "PUT" || LMethod === "POST" || LMethod === "PATCH")
+            {
+                LRequest.send(APayLoad);
+            }
+
+        }
+        catch (Error)
+        {
+            alert(`An error occured during execution: ${Error.message}`);
+            console.error(`[RestClient].[Execute]: An error has been thrown: ${Error.message}`);
+        }
+
+    }
+
+}

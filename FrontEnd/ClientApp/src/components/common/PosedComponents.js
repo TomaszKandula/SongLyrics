0354bf249cb1d98f680ad78cb58b6f23181fa1bb
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-09-10 12:25:45 +0200

    add posed components

---
+++ FrontEnd/ClientApp/src/components/common/PosedComponents.js
@@ -0,0 +1,38 @@
+﻿import posed from "react-pose";
+
+// common opacity animation
+export const fadeAnimation =
+{
+    exit: { opacity: 0 },
+    enter: {
+        opacity: 1,
+        delay: 100,
+        beforeChildren: true,
+        delayChildren: 200,
+        staggerChildren: 50,
+        transition: {
+            type: "tween",
+            duration: 500
+        }
+    }
+};
+
+// common scale + opacity animation
+export const scaledFadeAnimation =
+{
+    enter: {
+        scale: 1,
+        opacity: 1
+    },
+    exit: {
+        scale: 1.2,
+        opacity: 0
+    }
+};
+
+export const MainContainer = posed.div(fadeAnimation);
+export const PosedTitle = posed.h1(scaledFadeAnimation);
+export const PosedDiv = posed.div(scaledFadeAnimation);
+export const PosedP = posed.p(scaledFadeAnimation);
+export const PosedLink = posed.a(scaledFadeAnimation);
+export const PosedFadeDiv = posed.div(fadeAnimation);

5db00fe08b90a55d951f23fe284afd8afe40b0d1
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-09-16 12:21:41 +0200

    split rendering from processing

--- FrontEnd/ClientApp/src/components/common/bandDetails.js
+++ FrontEnd/ClientApp/src/components/common/bandDetails.js
@@ -54,23 +54,22 @@ class BandDetails extends Component
 
     }      
 
-    renderTable()
+    processData()
     {
 
-        let details = this.state.details;
         let pastMembers = "";
         let currMembers = "";
 
-        for (let Index = 0; Index < details.members.length; ++Index)
+        for (let Index = 0; Index < this.state.details.members.length; ++Index)
         {
 
-            if (details.members[Index].Status === "Active")
+            if (this.state.details.members[Index].Status === "Active")
             {
-                currMembers = currMembers + ` ${details.members[Index].FirstName} ${details.members[Index].LastName},`;
+                currMembers = currMembers + ` ${this.state.details.members[Index].FirstName} ${this.state.details.members[Index].LastName},`;
             }
             else
             {
-                pastMembers = pastMembers + ` ${details.members[Index].FirstName} ${details.members[Index].LastName},`;
+                pastMembers = pastMembers + ` ${this.state.details.members[Index].FirstName} ${this.state.details.members[Index].LastName},`;
             }
 
         }
@@ -78,42 +77,57 @@ class BandDetails extends Component
         currMembers = currMembers.replace(/.$/, ".");
         pastMembers = pastMembers.replace(/.$/, ".");
 
-        let getEstablished = new Date(details.established);
-        let getActiveUntil = new Date(details.activeUntil);
+        let getEstablished = new Date(this.state.details.established);
+        let getActiveUntil = new Date(this.state.details.activeUntil);
 
-        let established = details.established != null ? getEstablished.getFullYear() : null;
-        let activeUntil = details.activeUntil != null ? getActiveUntil.getFullYear() : null;
+        let established = this.state.details.established != null ? getEstablished.getFullYear() : null;
+        let activeUntil = this.state.details.activeUntil != null ? getActiveUntil.getFullYear() : null;
+
+        let processed =
+        {
+            established: established,
+            activeUntil: activeUntil,
+            currMembers: currMembers,
+            pastMembers: pastMembers
+        };
+
+        return processed;
+
+    }
+
+    renderTable(processed)
+    {
 
         return (
             <table className="detailsTable">
                 <tbody>
                     <tr>
                         <td colSpan="2">
-                            <h3><b>{details.name}</b></h3>
+                            <h3><b>{this.state.details.name}</b></h3>
                         </td>
                     </tr>
                     <tr>
                         <td className="detailsTableCol1">Established</td>
-                        <td className="detailsTableCol2">{established}</td>
+                        <td className="detailsTableCol2">{processed.established}</td>
                     </tr>
                     <tr>
                         <td className="detailsTableCol1">Active until</td>
-                        <td className="detailsTableCol2">{activeUntil}</td>
+                        <td className="detailsTableCol2">{processed.activeUntil}</td>
                     </tr>
                     <tr>
                         <td className="detailsTableCol1">Genere</td>
-                        <td className="detailsTableCol2">{details.genere}</td>
+                        <td className="detailsTableCol2">{this.state.details.genere}</td>
                     </tr>
                     <tr>
                         <td colSpan="2">Members</td>
                     </tr>
                     <tr>
                         <td className="detailsTableCol1">Current</td>
-                        <td className="detailsTableCol2">{currMembers}</td>
+                        <td className="detailsTableCol2">{processed.currMembers}</td>
                     </tr>
                     <tr>
                         <td className="detailsTableCol1">Past</td>
-                        <td className="detailsTableCol2">{pastMembers}</td>
+                        <td className="detailsTableCol2">{processed.pastMembers}</td>
                     </tr>
 
                 </tbody>
@@ -125,7 +139,7 @@ class BandDetails extends Component
     render()
     {
 
-        let renderedTable = this.state.loading ? <Loaders.Circular /> : this.renderTable();
+        let renderedTable = this.state.loading ? <Loaders.Circular /> : this.renderTable(this.processData());
 
         return (
             <div>
